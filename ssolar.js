51d34117b1da32c2845119146a755e774a707673
GeGeFe <gabrielgf2002@yahoo.com.ar>
2024-08-11 20:45:40 -0300

    Un botón.

--- ssolar.js
+++ ssolar.js
@@ -7,3 +7,13 @@ AFRAME.registerComponent('clickm1', {
     })
   }
 });
+window.onload = function () {
+    document
+      .querySelector(".unboton")
+      .addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("¡Hola!");
+      });
+  };

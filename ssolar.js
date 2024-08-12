7961ec345c5cc3e4fe483075c8112ee25527dce6
GeGeFe <gabrielgf2002@yahoo.com.ar>
2024-08-11 21:05:42 -0300

    Funciona la detección de eventos.

--- ssolar.js
+++ ssolar.js
@@ -2,8 +2,8 @@ AFRAME.registerComponent('clickm1', {
   init: function () {
     this.el.sceneEl.addEventListener('markerFound', () => {
       // redirect to custom URL e.g. google.com
-      console.log("¡Holaaaaa!");
-      window.location = 'https://www.google.com/';
+      console.log("¡Detecté a Mercurio!");
+ //     window.location = 'https://www.google.com/';
     })
   }
 });
